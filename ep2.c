c796e0a0b310232e7cc71ded793749422ba35863
gustavo84171 <gustavo84171@hotmail.com>
2016-10-03 16:39:10 -0400

    Modo 1 e perfumarias terminado

--- ep2.c
+++ ep2.c
@@ -42,6 +42,7 @@ void *dummy ();
 void waitForSync ();
 void manageRace ();
 void breakCyclist ();
+int comparator (const void *a, const void *b);
 
 /*      Variáveis globais       */
 SLOT *pista;
@@ -59,7 +60,7 @@ int timeToBreak;
 pthread_mutex_t lapmutex;
 
 int main (int argc, char **argv) {
-    int n, i;
+    int n, i, winners[2], thirdTick[2];
     char type;
     ARGS* thread_arg;
     if (argc < 4) exit (EXIT_FAILURE);
@@ -102,7 +103,7 @@ int main (int argc, char **argv) {
             c[i].vel = 60;
             c[i].team = 1;
             c[i].lap = 0;
-            c[i].pos = 0;
+            c[i].pos = d/2;
             c[i].id = i + 1;
             c[i].finalTick = 0;
             c[i].broken = 0;
@@ -131,12 +132,44 @@ int main (int argc, char **argv) {
         /* Esperando retornar*/
         for (i = 0; i < 2 * n; i++)
             pthread_join (c[i].thread, NULL);
+        
+        /* após o fim da corrida */
+        printf("A corrida terminou!\n\nRanking:\n");
+        
+        /* ordenar o ranking de ciclistas */
+        qsort(c, 2*n, sizeof(*c), comparator);
+        winners[0] = winners[1] = thirdTick[0] = thirdTick[1] = 0;
+        
+        for(i = 0; i < cycsize; i++) {
+            
+            /* vamos contabilizar qual equipe venceu a corrida */
+            winners[c[i].team]++;
+            
+            /* pra isso, temos que ver qual terceiro ciclista acabou antes */
+            if(winners[c[i].team] == 3)
+                thirdTick[c[i].team] = c[i].finalTick;
+            
+            /* exibindo ranking */
+            if(c[i].broken)
+                printf("%do: Ciclista %d (Equipe %d) - ACIDENTE (Volta %d)\n", i+1, c[i].id, c[i].team+1, c[i].lap+1);
+            else
+                printf("%do: Ciclista %d (Equipe %d) - %.3fs\n", 
+                   i+1, c[i].id, c[i].team+1, (float) (c[i].finalTick*60)/1000);
+        }
+        
+        /* exibimos o print da vitória, pra terminar o EP */
+        if(thirdTick[0] < thirdTick[1])
+            printf("\nVitória da equipe 1!\n\n");    
+        else if(thirdTick[0] > thirdTick[1])
+            printf("\nVitória da equipe 2!\n\n");
+        else
+            printf("\nEmpate!\n\n");
     }
     return 0;
 }
 
 void *ciclista (void *a) {
-    int i, j, passedLap, nextPos, random;
+    int i, j, passedLap, nextPos;
     int clock1, clock2, index1, index2, first, second;
     pthread_t dummyid;
     ARGS* p = (ARGS *) a;
@@ -162,7 +195,7 @@ void *ciclista (void *a) {
                 c[i].pos = nextPos;
 
                 /* verificar se houve um incremento de volta */
-                if (nextPos == 0) {
+                if ((c[i].team == 0 && nextPos == 0) || (c[i].team == 1 && nextPos == d/2)) {
                     c[i].lap++;
                     
                     /* verificamos se ele é o primeiro a mudar de volta */
@@ -190,12 +223,15 @@ void *ciclista (void *a) {
                         if(c[j].broken) continue;
                         
                         /* guardamos tambem o primeiro e segundo colocados */
-                        if (c[j].team == c[i].team && c[j].lap == c[i].lap && i != j) {
+                        if (c[j].team == c[i].team && c[j].lap >= c[i].lap && i != j) {
                             if (clock1 == 0) {
-                                clock1 = (c[j].lap * d) + c[j].pos;
+                                
+                                /* O ultimo fator da soma corrige a diferença do ponto de largada
+                                   das duas equipes */
+                                clock1 = (c[j].lap * d) + c[j].pos - c[j].team*(d/2);
                                 index1 = c[j].id;
                             } else {
-                                clock2 = (c[j].lap * d) + c[j].pos;
+                                clock2 = (c[j].lap * d) + c[j].pos - c[j].team*(d/2);
                                 index2 = c[j].id;
                             }
                             passedLap++;
@@ -306,11 +342,6 @@ void manageRace () {
             break;
         }
     }
-
-    /* se a corrida acabou, imprimimos o ranking */
-    if(raceEnd) {
-
-    }
 }
 
 /* rola um dado e com chance de 10% quebra algum ciclista */
@@ -349,4 +380,35 @@ void breakCyclist () {
         printf("Ciclista %d sofreu um acidente!\n\n", c[lucky].id);
         runners[c[lucky].team]--;
     }
+}
+
+/* função que ordena os ciclistas por ordem de chegada,
+   comparando dois a dois:
+   - retorna x < 0 se A deve vir antes de B
+   - retorna X > 0 se B deve vir antes de A
+   - retorna X = 0 se são iguais
+   */
+int comparator (const void *a, const void *b) {
+    CYCLIST *c1, *c2;
+    int dist1, dist2;
+        
+    /* fazemos o cast para ponteiro de ciclista */
+    c1 = (CYCLIST*) a;
+    c2 = (CYCLIST*) b;
+    
+    /* se ambos estiverem quebrados, predomina o lap em que quebraram */ 
+    if(c1->broken && c2->broken)
+        return c2->lap - c1->lap;
+    
+    /* se apenas um estiver quebrado, ele vai pro fundo da lista */
+    if(c1->broken)
+        return 1;
+    if(c2->broken)
+        return -1;
+    
+    /* a comparação tradicional é por distancia percorrida,
+       ja que mesmo apos passar a linha de chegada eles continuam andando */
+    dist1 = c1->lap*d + c1->pos - c1->team*(d/2);
+    dist2 = c2->lap*d + c2->pos - c2->team*(d/2);
+    return dist2 - dist1;
 }
