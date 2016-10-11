46fa8957eaba8bc518e979764621fd8935e80a23
Leonardo Padilha <lcfpadilha@gmail.com>
2016-10-11 17:18:50 -0300

    deixando codigo lustroso

---
+++ testes/run_tests.sh
@@ -0,0 +1,9 @@
+for i in `seq 1 30`; do
+	#pista pequena / qtd de ciclistas pequeno
+
+	#pista pequena / qtd de ciclistas medio
+
+	#pista pequena / qtd de ciclistas grande
+
+	#pista media / qtd	
+done
